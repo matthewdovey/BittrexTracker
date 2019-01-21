cb21b4af1b18ab8487a046864feb23cdff009311
matthew dovey <matthewdovey@hotmail.co.uk>
2019-01-21 22:24:21 +0000

    Initial Commit

---
+++ BittrexTracker/BittrexTracker.h
@@ -0,0 +1,19 @@
+//
+//  BittrexTracker.h
+//  BittrexTracker
+//
+//  Created by Matthew Dovey on 21/01/2019.
+//  Copyright © 2019 Matthew Dovey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BittrexTracker.
+FOUNDATION_EXPORT double BittrexTrackerVersionNumber;
+
+//! Project version string for BittrexTracker.
+FOUNDATION_EXPORT const unsigned char BittrexTrackerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BittrexTracker/PublicHeader.h>
+
+
