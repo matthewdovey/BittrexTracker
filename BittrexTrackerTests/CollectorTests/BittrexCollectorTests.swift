69c54ce158cf9824d01803e71f870ef932e24246
Matthew  Dovey <matthewdovey@coderus.com>
2019-01-22 15:00:38 +0000

    tests: addition of collector test group and bittrex collector test class

---
+++ BittrexTrackerTests/CollectorTests/BittrexCollectorTests.swift
@@ -0,0 +1,18 @@
+//
+//  BittrexCollectorTests.swift
+//  BittrexTrackerTests
+//
+//  Created by Matthew  Dovey on 22/01/2019.
+//  Copyright © 2019 Matthew Dovey. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class BittrexCollectorTests : XCTestCase {
+    
+    override func setUp() {
+        //TODO: setup properties and objects for tests
+    }
+    
+}
