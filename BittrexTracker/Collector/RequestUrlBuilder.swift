0aa318d65233d5a8940f33ce85ef1f0a25720f46
matthew dovey <matthewdovey@hotmail.co.uk>
2019-01-30 21:29:43 +0000

    feat: addition of market urls to builder and doc updates

--- BittrexTracker/Collector/RequestUrlBuilder.swift
+++ BittrexTracker/Collector/RequestUrlBuilder.swift
@@ -8,17 +8,20 @@
 
 import Foundation
 
-/// A class to provide the request urls
+/// Class to provide the request URLS for public, account and market requests
 final class RequestUrlBuilder {
   
-  // URL constants (public requests)
+  // Base URL components
   private let baseURL = "https://bittrex.com/api/"
   private let apiVersion = "v1.1"
+  
+  // URL constants (public requests)
   private let marketsURL = "/public/getmarkets"
   private let currenciesURL = "/public/getcurrencies"
-  private let marketSummariesURL = "/public/getmarketsummaries"
   private let tickerURL = "/public/getticker?market="
   private let marketSummaryURL = "/public/getmarketsummary?market="
+  private let marketSummariesURL = "/public/getmarketsummaries"
+  private let orderbookURL = "/public/getorderbook"
   private let marketHistoryUrl = "/public/getmarkethistory?market="
   
   // URL constants (account requests)
@@ -27,6 +30,12 @@ final class RequestUrlBuilder {
   private let orderHistoryURL = "/account/getorderhistory"
   private let depositHistoryURL = "/account/getdeposithistory"
   private let withdrawalHistoryURL = "/account/getwithdrawalhistory"
+  
+  // URL constants (market requests)
+  private let buyLimitURL = "/market/buylimit"
+  private let sellLimitURL = "/market/selllimit"
+  private let cancelURL = "/market/cancel"
+  private let openOrdersURL = "/market/getopenorders"
   
   // Parameter constants
   private let currencyParam = "?currency="
@@ -41,12 +50,11 @@ final class RequestUrlBuilder {
   private var apiSecret: String
   private var apiSecretBytes: [UInt8]?
   
-  // TODO: finish description for api secret docs
   /// Initialiser to set the API key and API secret
   ///
   /// - Parameters:
-  ///   - apiKey: the api key for the user's wallet
-  ///   - apiSecret:
+  ///   - apiKey: The user's API key
+  ///   - apiSecret: The user's API secret
   init(key: String = "", secret: String = "") {
     self.apiKey = key
     self.apiSecret = secret
@@ -54,22 +62,22 @@ final class RequestUrlBuilder {
   
   /// API key property setter
   ///
-  /// - Parameter apiKey: the API key
+  /// - Parameter apiKey: The API key
   public func setKey(key: String) {
     self.apiKey = key
   }
   
   /// API secret property setter
   ///
-  /// - Parameter apiSecret: the API secret
+  /// - Parameter apiSecret: The API secret
   public func setSecret(secret: String) {
     self.apiSecret = secret
   }
   
   /// Method to take a request URL enum and pass the correlating url string
   ///
-  /// - Parameter request: a request URL enum to define the url to return
-  /// - Returns: a request url to be used with the Bittrex API
+  /// - Parameter request: Request URL enum to define the URL to return
+  /// - Returns: Request URL to be used with the Bittrex API
   public func buildUrlFor(request: RequestUrl) -> String {
     switch request {
     case .Currencies:
