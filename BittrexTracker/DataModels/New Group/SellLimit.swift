9adc0a591e1cbf2a32a2b9db8b3cd07a795322c4
matthew dovey <matthewdovey@hotmail.co.uk>
2019-01-31 21:29:08 +0000

    feat: creation of market data model group and creation of market models

---
+++ BittrexTracker/DataModels/New Group/SellLimit.swift
@@ -0,0 +1,13 @@
+//
+//  SellLimit.swift
+//  BittrexTracker
+//
+//  Created by Matthew Dovey on 31/01/2019.
+//  Copyright © 2019 Matthew Dovey. All rights reserved.
+//
+
+import Foundation
+
+struct SellLimit: Decodable {
+  
+}
