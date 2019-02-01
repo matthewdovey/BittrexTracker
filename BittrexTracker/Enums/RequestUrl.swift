ce460e34b22e4ebe6a134d4564bd4bc8b8164a93
matthew dovey <matthewdovey@hotmail.co.uk>
2019-02-01 00:23:22 +0000

    feat: addition of last request enums

--- BittrexTracker/Enums/RequestUrl.swift
+++ BittrexTracker/Enums/RequestUrl.swift
@@ -10,39 +10,43 @@ import Foundation
 
 /// Request URL enum to allow for easier selection of required URL
 ///
-/// - Currencies: The currencies request URL
 /// - Markets: The markets request URL
-/// - MarketSummaries: The market summaries request URL
+/// - Currencies: The currencies request URL
 /// - Ticker: The ticker request URL
+/// - MarketSummaries: The market summaries request URL
 /// - MarketSummary: The market summary request URL
-/// - MarketHistory: The market history request URL
 /// - OrderBook: The order book request URL
-/// - Balance: The balance request URL
-/// - Balances: The balances request URL
-/// - DepositAddress: The deposit address request URL
-/// - OrderHistory: The order history request URL
-/// - WithdrawalHistory: The withdrawal history request URL
-/// - DepositHistory: The deposit history request URL
+/// - MarketHistory: The market history request URL
 /// - BuyLimit: The buy limit request URL
 /// - SellLimit: The sell limit request URL
 /// - Cancel: The cancel request URL
 /// - OpenOrders: The open orders request URL
+/// - Balances: The balances request URL
+/// - Balance: The balance request URL
+/// - DepositAddress: The deposit address request URL
+/// - Withdraw: The withdraw request URL
+/// - Order: The order request URL
+/// - OrderHistory: The order history request URL
+/// - WithdrawalHistory: The withdrawal history request URL
+/// - DepositHistory: The deposit history request URL
 enum RequestUrl {
-  case Currencies
   case Markets
-  case MarketSummaries
+  case Currencies
   case Ticker
+  case MarketSummaries
   case MarketSummary
-  case MarketHistory
   case OrderBook
-  case Balance
-  case Balances
-  case DepositAddress
-  case OrderHistory
-  case WithdrawalHistory
-  case DepositHistory
+  case MarketHistory
   case BuyLimit
   case SellLimit
   case Cancel
   case OpenOrders
+  case Balances
+  case Balance
+  case DepositAddress
+  case Withdraw
+  case Order
+  case OrderHistory
+  case WithdrawalHistory
+  case DepositHistory
 }
