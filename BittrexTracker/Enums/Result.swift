aef11a4928d964ebf408a9ea06f711f834bbf75c
Matthew  Dovey <matthewdovey@coderus.com>
2019-02-08 17:20:56 +0000

    feat: creation of result enum

---
+++ BittrexTracker/Enums/Result.swift
@@ -0,0 +1,14 @@
+//
+//  Result.swift
+//  BittrexTracker
+//
+//  Created by Matthew  Dovey on 08/02/2019.
+//  Copyright © 2019 Matthew Dovey. All rights reserved.
+//
+
+import Foundation
+
+enum Result {
+  case success
+  case failure
+}
