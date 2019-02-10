0182ee53042510a7177cd3d840aaa46cd8fdfdd3
matthew dovey <matthewdovey@hotmail.co.uk>
2019-02-10 15:31:24 +0000

    feat: addition of all public method bodies

--- BittrexTracker/BittrexManager.swift
+++ BittrexTracker/BittrexManager.swift
@@ -8,15 +8,15 @@
 
 import Foundation
 
-///
+/// Class to provide access to all Bittrex API wrappers
 final class BittrexManager {
 
   private let publicCollector: PublicCollector
   private let session: URLSession
   private let urlBuilder: RequestUrlBuilder
 
-  ///
-  init() {
+  /// Initialiser to create all collectors and their required dependencies
+  public init() {
     session = URLSession.shared
     urlBuilder = RequestUrlBuilder()
     publicCollector = PublicCollector(session: session, builder: urlBuilder)
@@ -24,41 +24,76 @@ final class BittrexManager {
 
   /// Method to return all currencies listed on the exchange
   ///
-  /// - Parameter completion: Escaping CoinRequest object
+  /// - Parameter completion: Escaping CoinResult object
   public final func getCurrencies(completion: @escaping ((CoinResult) -> Void)) {
-
+    publicCollector.getCurrencies { (request) in
+      if request.success == true, let data = request.result {
+        completion(CoinResult(outcome: .success, data: data))
+      } else {
+        let message = request.message ?? "unknown"
+        completion(CoinResult(outcome: .failure(message), data: []))
+      }
+    }
   }
 
   /// Method to return all markets for currencies
   ///
-  /// - Parameter completion: Escaping MarketsRequest object
+  /// - Parameter completion: Escaping MarketsResult object
   public final func getMarkets(completion: @escaping ((MarketResult) -> Void)) {
-
+    publicCollector.getMarkets { (request) in
+      if request.success == true, let data = request.result {
+        completion(MarketResult(outcome: .success, data: data))
+      } else {
+        let message = request.message ?? "unknown"
+        completion(MarketResult(outcome: .failure(message), data: []))
+      }
+    }
   }
 
   /// Method to return ticker results for a specified currency
   ///
   /// - Parameters:
   ///   - market: The market to retrieve data for
-  ///   - completion: Escaping TickerRequest object
+  ///   - completion: Escaping TickerResult object
   public final func getTickerFor(market: String, completion: @escaping ((TickerResult) -> Void)) {
-
+    publicCollector.getTickerFor(market: market, completion: { (request) in
+      if request.success == true, let data = request.result {
+        completion(TickerResult(outcome: .success, data: [data]))
+      } else {
+        let message = request.message ?? "unknown"
+        completion(TickerResult(outcome: .failure(message), data: []))
+      }
+    })
   }
 
   /// Method to return all market summaires
   ///
-  /// - Parameter completion: Escaping MarketSummaryRequest object
+  /// - Parameter completion: Escaping MarketSummaryResult object
   public final func getMarketSummaries(completion: @escaping ((MarketSummaryResult) -> Void)) {
-
+    publicCollector.getMarketSummaries { (request) in
+      if request.success == true, let data = request.result {
+        completion(MarketSummaryResult(outcome: .success, data: data))
+      } else {
+        let message = request.message ?? "unknown"
+        completion(MarketSummaryResult(outcome: .failure(message), data: []))
+      }
+    }
   }
 
   /// Method to return a market summary for a specified currency
   ///
   /// - Parameters:
   ///   - market: The market to retrieve data for
-  ///   - completion: Escaping MarketSummaryRequest object
+  ///   - completion: Escaping MarketSummaryResult object
   public final func getSummaryForMarket(market: String, completion: @escaping ((MarketSummaryResult) -> Void)) {
-
+    publicCollector.getSummaryForMarket(market: market, completion: { (request) in
+      if request.success == true, let data = request.result {
+        completion(MarketSummaryResult(outcome: .success, data: data))
+      } else {
+        let message = request.message ?? "unknown"
+        completion(MarketSummaryResult(outcome: .failure(message), data: []))
+      }
+    })
   }
 
   /// Method to return the order book for a specified currency
@@ -66,17 +101,32 @@ final class BittrexManager {
   /// - Parameters:
   ///   - market: The market to retrieve data for
   ///   - type: Orderbook type
-  ///   - completion: Escaping OrderBookRequest object
+  ///   - completion: Escaping OrderBookResult object
   public final func getOrderBook(market: String, type: String, completion: @escaping ((OrderBookResult) -> Void)) {
-
+    publicCollector.getOrderBook(market: market, type: type, completion: { (request) in
+      // TODO: setup order book request object to have a result property
+      if request.success == true/*, let data = request.result*/ {
+        completion(OrderBookResult(outcome: .success, data: []))
+      } else {
+        let message = request.message ?? "unknown"
+        completion(OrderBookResult(outcome: .failure(message), data: []))
+      }
+    })
   }
 
   /// Method to retrieve the market history for a specified currency
   ///
   /// - Parameters:
   ///   - market: The market to retrieve data for
-  ///   - completion: Escaping MarketHistoryRequest object
+  ///   - completion: Escaping MarketHistoryResult object
   public final func getMarketHistoryFor(market: String, completion: @escaping ((MarketHistoryResult) -> Void)) {
-    
+    publicCollector.getMarketHistoryFor(market: market, completion: { (request) in
+      if request.success == true, let data = request.result {
+        completion(MarketHistoryResult(outcome: .success, data: data))
+      } else {
+        let message = request.message ?? "unknown"
+        completion(MarketHistoryResult(outcome: .failure(message), data: []))
+      }
+    })
   }
 }
