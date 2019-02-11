c93b767368b322a40acdd1e2bebe6ae227a4d58d
matthew dovey <matthewdovey@hotmail.co.uk>
2019-02-11 00:49:36 +0000

    feat: creation of account result models

---
+++ BittrexTracker/ResultModels/Account/OrderHistoryResult.swift
@@ -0,0 +1,9 @@
+//
+//  OrderHistoryResult.swift
+//  BittrexTracker
+//
+//  Created by Matthew Dovey on 11/02/2019.
+//  Copyright © 2019 Matthew Dovey. All rights reserved.
+//
+
+import Foundation
