2a9e5d561baf3499f16c41cc53630ca28e245b92
matthewdovey <matthewdovey@coderus.com>
2019-02-13 13:32:25 +0000

    feat: creation of placeholder enum for new url builder structure

---
+++ BittrexTracker/Enums/Placeholder.swift
@@ -0,0 +1,21 @@
+//
+//  Placeholder.swift
+//  BittrexTracker
+//
+//  Created by Matthew  Dovey on 13/02/2019.
+//  Copyright © 2019 Matthew Dovey. All rights reserved.
+//
+
+import Foundation
+
+enum Placeholder: String {
+  case apiKey = "API_KEY"
+  case market = "MARKET"
+  case quantity = "QUANTITY"
+  case type = "TYPE"
+  case rate = "RATE"
+  case uuid = "UUID"
+  case currency = "CURRENCY"
+  case address = "ADDRESS"
+  case paymentid = "PAYMENT_ID"
+}
