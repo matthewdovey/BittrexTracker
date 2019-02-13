9424579317b57208b6fff7349a2819b77e05f5bf
matthew dovey <matthewdovey@hotmail.co.uk>
2019-02-13 22:47:19 +0000

    docs: addition of docs to enums

--- BittrexTracker/Enums/Placeholder.swift
+++ BittrexTracker/Enums/Placeholder.swift
@@ -8,6 +8,17 @@
 
 import Foundation
 
+/// Placeholder enum to provide the string placeholder for each type
+///
+/// - apiKey: The API key placeholder
+/// - market: The market value placeholder
+/// - quantity: The quantity value placeholder
+/// - type: The type value placeholder
+/// - rate: The rate value placeholder
+/// - uuid: The uuid value placeholder
+/// - currency: The currency value placeholder
+/// - address: The address value placeholder
+/// - paymentid: The payment ID placeholder
 enum Placeholder: String {
   case apiKey = "API_KEY"
   case market = "MARKET"
