298ed3a9f6e009883fbde230ffad0fe9a993a33f
matthew dovey <matthewdovey@hotmail.co.uk>
2019-02-18 19:46:35 +0000

    feat: addition of test class and test method shells

--- BittrexTrackerTests/BittrexManagerTests.swift
+++ BittrexTrackerTests/BittrexManagerTests.swift
@@ -6,4 +6,72 @@
 //  Copyright © 2019 Matthew Dovey. All rights reserved.
 //
 
-import Foundation
+import XCTest
+@testable import BittrexTracker
+
+class BittrexManagerTests: XCTestCase {
+  
+  var bittrexManager: BittrexManager!
+  
+  override func setUp() {
+    bittrexManager = BittrexManager()
+  }
+  
+  // MARK: public API calls
+  
+  func testGetCurrenciesReturnsSuccess() {
+  
+  }
+  
+  func testGetCurrenciesReturnsFailure() {
+    
+  }
+  
+  func testGetMarketsReturnSuccess() {
+    
+  }
+  
+  func testGetMarketsReturnsFailure() {
+    
+  }
+  
+  func testGetTickerReturnsSuccess() {
+    
+  }
+  
+  func testGetTickerReturnsFailure() {
+    
+  }
+  
+  func testGetMarketSummariesReturnsSuccess() {
+    
+  }
+  
+  func testGetMarketSummariesReturnsFailure() {
+    
+  }
+  
+  func testGetMarketSummaryForReturnsSuccess() {
+    
+  }
+  
+  func testGetMarketSummaryForReturnsFailure() {
+    
+  }
+  
+  func testGetOrderBookReturnsSuccess() {
+    
+  }
+  
+  func testGetOrderBookReturnsFailure() {
+    
+  }
+  
+  func testGetMarketHistoryReturnsSuccess() {
+    
+  }
+  
+  func testGetMarketHistoryReturnsFailure() {
+    
+  }
+}
