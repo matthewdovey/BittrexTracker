df0af70519dade76988a670598f88c66abac6941
matthew dovey <matthewdovey@hotmail.co.uk>
2019-02-18 19:45:59 +0000

    feat: addition of bittrex manager tests class

---
+++ BittrexTrackerTests/BittrexManagerTests.swift
@@ -0,0 +1,9 @@
+//
+//  BittrexManagerTests.swift
+//  BittrexTrackerTests
+//
+//  Created by Matthew Dovey on 18/02/2019.
+//  Copyright © 2019 Matthew Dovey. All rights reserved.
+//
+
+import Foundation
