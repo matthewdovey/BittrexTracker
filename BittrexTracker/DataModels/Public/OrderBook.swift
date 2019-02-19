0fee7fad0bb71d2b4b1e1423c4a0610d7fe65a61
matthew dovey <matthewdovey@hotmail.co.uk>
2019-02-19 19:28:21 +0000

    feat: creation of transaction structs to be contained by an order book object

--- BittrexTracker/DataModels/Public/OrderBook.swift
+++ BittrexTracker/DataModels/Public/OrderBook.swift
@@ -9,12 +9,23 @@
 import Foundation
 
 /// The data structure for the result of the Order Book Request
-public struct OrderBook {
-  public let buy: [String : Float]?
-  public let sell: [String : Float]?
+public struct OrderBook: Decodable {
+  public let buy: [Transaction]?
+  public let sell: [Transaction]?
   
   private enum CodingKeys: String, CodingKey {
     case buy = "buy"
     case sell = "sell"
+  }
+}
+
+// The data structure for the Transaction objects within an Order Book Request
+public struct Transaction: Decodable {
+  public let quantity: Float?
+  public let rate: Float?
+  
+  private enum CodingKeys: String, CodingKey {
+    case quantity = "quantity"
+    case rate = "rate"
   }
 }
