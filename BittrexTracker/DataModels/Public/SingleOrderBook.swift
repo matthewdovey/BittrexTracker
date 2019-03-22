df388f44ce96946dcc704cfd610d473f60a8336d
matthew dovey <matthewdovey@hotmail.co.uk>
2019-03-22 23:18:34 +0000

    feat: addition of single order book model

---
+++ BittrexTracker/DataModels/Public/SingleOrderBook.swift
@@ -0,0 +1,19 @@
+//
+//  SingleOrderBook.swift
+//  BittrexTracker
+//
+//  Created by Matthew Dovey on 22/03/2019.
+//  Copyright © 2019 Matthew Dovey. All rights reserved.
+//
+
+import Foundation
+
+public struct SingleOrderBook: Decodable {
+  public let quantity: Float?
+  public let rate: Float?
+  
+  private enum CodingKeys: String, CodingKey {
+    case quantity = "quantity"
+    case rate = "rate"
+  }
+}
