2419e341d590b4af7fe2b467057f396457439cf3
matthew dovey <matthewdovey@hotmail.co.uk>
2019-03-23 01:08:33 +0000

    feat: creation of order book type enum

---
+++ BittrexTracker/Enums/OrderBookType.swift
@@ -0,0 +1,18 @@
+//
+//  OrderBookType.swift
+//  BittrexTracker
+//
+//  Created by Matthew Dovey on 23/03/2019.
+//  Copyright © 2019 Matthew Dovey. All rights reserved.
+//
+
+import Foundation
+
+/// OrderBookType enum to specify the order book to retrieve
+///
+/// - sell: The sell order book
+/// - buy: The buy order book
+enum OrderBookType {
+  case sell
+  case buy
+}
